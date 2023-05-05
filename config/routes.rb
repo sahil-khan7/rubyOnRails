00d917fc76bd72bec86fd19025745c2a9b642af6
Sahil Khan <sahil.khan@deeporion>
2023-05-05 18:42:25 +0530

    CRUD operation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "articles#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
